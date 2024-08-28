609c83be9c7282b9a935d46ab88a7e95d9c2ad81
Ulianaa56 <147165483+Ulianaa56@users.noreply.github.com>
2024-08-28 19:15:13 +0200

    слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+let clicked = 0;
+const slider = document.querySelector('.example-slider__comp');
+const img = document.querySelector('.example-slider__comp-img--overlay');
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,3 +16,55 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+function slide(x) {
+  img.style.width = x + "px";
+  slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+}
+
+function getCursorPos(e) {
+  const a = 0, x = 0;
+  e = (e.changedTouches) ? e.changedTouches[0] : e;
+  a = img.getBoundingClientRect();
+  x = e.pageX - a.left;
+  x = x - window.pageXOffset;
+  return x;
+}
+
+function slideMove(e) {
+  const pos = 0;
+  if (clicked == 0) return false;
+  pos = getCursorPos(e)
+  if (pos < 0) pos = 0;
+  if (pos > w) pos = w;
+  slide(pos);
+}
+
+function slideReady(e) {
+  e.preventDefault();
+  clicked = 1;
+  window.addEventListener("mousemove", slideMove);
+
+}
+
+function slideFinish(e) {
+  clicked = 0;
+}
+
+
+function compareImages() {
+  const w = img.offsetWidth;
+  const h = img.offsetHeight;
+  img.style.width = (w / 2) + "px";
+  slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+  slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+  slider.addEventListener("mousedown", slideReady);
+  window.addEventListener("mouseup", slideFinish);
+}
+
+function initSlider() {
+  compareImages();
+}
+
+
+initSlider();
